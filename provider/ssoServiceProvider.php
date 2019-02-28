d9d6ef424dbe9b42c8af34d7d8e62d77354f547a
devasyadave <38454922+devasyadave@users.noreply.github.com>
2019-02-28 11:23:47 +0530

    Add files via upload

---
+++ provider/ssoServiceProvider.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace provider;
+
+use Illuminate\Support\ServiceProvider;
+
+class ssoServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+       
+        
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        require_once 'autoload.php';
+        $this->loadRoutesFrom(__DIR__.'/../src/routes.php');
+         $this->publishes([
+        __DIR__.'/../src/includes/css' => public_path('Miniorange/includes/css'),
+    ], 'public');
+         __DIR__.'/../src/includes/js/plugins' => public_path('Miniorange/includes/js/plugins'),
+    ], 'public');
+__DIR__.'/../src/resources/images' => public_path('Miniorange/resources/images'),
+    ], 'public');
+__DIR__.'/../src/resources' => public_path('Miniorange/resources'),
+    ], 'public');
+        //
+    }
+}
